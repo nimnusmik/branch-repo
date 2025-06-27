18b0eaf6e878f88bda0a032ea7489fc83eec72d1
nimnusmik <kimsunmin0227@gmail.com>
2025-06-27 11:15:33 +0900

    feat Print fizzbuzz if i is disible by 15, Print fizzbuzz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,7 +1,9 @@
-for j in rang(1,16+1):
-	if j % 5 == 0:
-		print('buzz)
-	elif j % 3 ==0:
+for i in range(1,16+1):
+	if i % 15 == 0: print('fizzbuzz')
+
+	elif i % 5 == 0:
+		print('buzz')
+	elif i % 3 ==0:
 		print('fizz')
 	else:
 		print(i)
